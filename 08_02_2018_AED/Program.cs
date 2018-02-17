ed1d678df4a2f51931b5826a28189334f7ac0831
K4YO <kayogamas@hotmail.com>
2018-02-17 11:17:28 -0200

    Final

--- 08_02_2018_AED/Program.cs
+++ 08_02_2018_AED/Program.cs
@@ -13,21 +13,103 @@ namespace _08_02_2018_AED
     {
         static void Main(string[] args)
         {
-            /*string op;
-            Console.WriteLine("Exercícios Interativos (com verções de recursivas) | Kayo Gamas | 601298 | 16/02/2018 \n\n");
-            Console.WriteLine(" 1 - Fatorial \n 1.1 - Fatorial Recursivo \n 2 - Fibonacci \n 2.2 - Fibonacci Recursivo \n 3a - Soma dos elementos de um Vetor \n " +
-                "3b - Maior elemento de um vetor \n 3b.1 - Maior elemento de um vetor Recursivo");
-            Console.Write("Digite uma opção: ");
-            op = Console.ReadLine();
-            switch (op)
+            Menu();         
+            
+        }
+
+        static void Menu()
+        {
+            bool sair = false;
+            while (!sair)
             {
-                case "1":
-                    Console.Write("Digite um número inteiro para o fatorial: ");
-                    int num = int.Parse(Console.ReadLine());
-                    Console.WriteLine("O fatorial de {0} é: {1}", num, FatorialIterativo(num));
-                    break;
+                string op;
+                int num;
+                string aux;
+                Console.Clear();
+                Console.WriteLine("Exercícios Interativos (com verções de recursivas) | Kayo Gamas | 601298 | 16/02/2018 \n\n");
+                Console.WriteLine(" 1 - Fatorial \n 1.1 - Fatorial Recursivo \n 2 - Fibonacci \n 2.2 - Fibonacci Recursivo \n 3a - Soma dos Elementos de um Vetor \n 3a.1 - Somar Recursiva dos Elementos de um Vetor \n " +
+                    "3b - Maior Elemento de um vetor \n 3b.1 - Maior elemento de um vetor Recursivo \n 4 - String sem Vogal \n 4 - String sem Vogal Recursivo");
+                Console.Write("Digite uma opção: ");
+                Console.WriteLine("\n");
+                op = Console.ReadLine();
+                switch (op)
+                {
+                    case "1":
+                        Console.Write("Digite um número inteiro para o fatorial: ");
+                        num = int.Parse(Console.ReadLine());
+                        Console.WriteLine("O fatorial de {0} é: {1}", num, FatorialIterativo(num));
+                        break;
+                    case "1.1":
+                        Console.Write("Digite um número inteiro para o fatorial recursivo: ");
+                        num = int.Parse(Console.ReadLine());
+                        Console.WriteLine("O fatorial de {0} é: {1}", num, FatorialRecursivo(num));
+                        break;
+                    case "2":
+                        Console.Write("Digite a posição para retornar o número respectivo da série Fibonacci: ");
+                        num = int.Parse(Console.ReadLine());
+                        Console.WriteLine("F({0}) = {1}", num, FibonacciIterativo(num));
+                        break;
+                    case "2.2":
+                        Console.Write("Digite a posição para retornar o número respectivo da série Fibonacci recursivo: ");
+                        num = int.Parse(Console.ReadLine());
+                        Console.WriteLine("F({0}) = {1}", num, FibonacciRecursivo(num));
+                        break;
+                    case "3a":
+                        Console.Write("Digite uma série de números inteiros separados por ';' (ponto e vírgula) para que seja retornado a soma: ");
+                        aux = Console.ReadLine();
+                        Console.WriteLine("A soma é: {0}", SomaElementosIterativo(ConvertToIntArray(aux)));
+                        break;
+                    case "3a.1":
+                        Console.Write("Digite uma série de números inteiros separados por ';' (ponto e vírgula) para que seja retornado a soma: ");
+                        aux = Console.ReadLine();
+                        Console.WriteLine("A soma recursiva é: {0}", SomaElementosRecursivo(ConvertToIntArray(aux), 0));
+                        break;
+                    case "3b":
+                        Console.Write("Digite uma série de números inteiros separados por ';' (ponto e vírgula) para que seja retornado o maior elemento: ");
+                        aux = Console.ReadLine();
+                        Console.WriteLine("O maior elemento da série é: {0}", MaiorElementoIterativo(ConvertToIntArray(aux)));
+                        break;
+                    case "3b.1":
+                        Console.Write("Digite uma série de números inteiros separados por ';' (ponto e vírgula) para que seja retornado o maior elemento: ");
+                        aux = Console.ReadLine();
+                        Console.WriteLine("O maior elemento da série recursivamente é: {0}", MaiorElementoRecursivo(ConvertToIntArray(aux), 0));
+                        break;
+                    case "4":
+                        Console.Write("Digite uma frase para que seja retornado a mesma, contudo, sem a(s) vogal(s):");
+                        aux = Console.ReadLine();
+                        Console.WriteLine("{0}", SemVogalIterativo(aux));
+                        break;
+                    case "4.1":
+                        Console.Write("Digite uma frase para que seja retornado a mesma, contudo, sem a(s) vogal(s) recursivamente:");
+                        aux = Console.ReadLine();
+                        Console.WriteLine("{0}", SemVogalRecursivo(aux, 0));
+                        break;
+                    default:
+                        Console.Write("\nEstá opção não existe, por favor digite qualquer tecla para voltar ao menu");
+                        Console.ReadKey();
+                        Menu();
+                        break;
+                }
+
+                Console.Write("\n\n'S' para sair, ou 'M' para voltar ao menu inicial:" );
+                aux = Console.ReadLine().ToLower();
+                if (aux == "s") sair = true;
+                else if (aux == "m") sair = false;
+
             }
-            */
+        }
+
+        static public int[] ConvertToIntArray(string aux)
+        {
+            string[] auxArray;
+            int[] numArray;
+            auxArray = aux.Split(';');
+            numArray = new int[auxArray.Length];
+            for (int i = 0; i < auxArray.Length; i++)
+            {
+                numArray[i] = Convert.ToInt32(auxArray[i]);
+            }
+            return numArray;
 
         }
         static public int FatorialIterativo(int n)
@@ -40,7 +122,7 @@ namespace _08_02_2018_AED
             }
             return result;
         }
-        public int FatorialRecursivo(int n)
+        static public int FatorialRecursivo(int n)
         {
             if ((n == 1) || (n == 0)) return 1;
             else return n * FatorialRecursivo(n - 1);
@@ -118,6 +200,21 @@ namespace _08_02_2018_AED
             }
             return soma;
         }
+
+        static public int SomaElementosRecursivo(int[] vet, int pos)
+        {
+            if (pos == vet.Length - 1)
+            {
+                return vet[pos];
+            }
+            else
+            {
+                int soma = vet[pos] + SomaElementosRecursivo(vet, (pos + 1));
+                return soma;
+               
+            }
+        }
+
         static public int MaiorElementoIterativo(int[] vet)
         {
             int num = vet[0];
